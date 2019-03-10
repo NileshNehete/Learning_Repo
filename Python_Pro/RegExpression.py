85be95339a84d1042cd0830740855aeb879ba5e4
Nilesh Nehete <nehete.nilesh@gmail.com>
2019-03-10 16:15:33 +0530

    new py files added

---
+++ Python_Pro/RegExpression.py
@@ -0,0 +1,37 @@
+# -*- coding: cp1252 -*-
+import re
+phonebook = ('9850984430','9960633284','98er98443O','99950')
+ 
+matchnum = re.findall("\d{10}",str(phonebook))
+print "below matched phone numbers are found"
+for phone in matchnum:
+    print phone
+
+
+
+'''
+abc…	Letters
+123…	Digits
+\d	Any Digit
+\D	Any Non-digit character
+.	Any Character
+\.	Period
+[abc]	Only a, b, or c
+[^abc]	Not a, b, nor c
+[a-z]	Characters a to z
+[0-9]	Numbers 0 to 9
+\w	Any Alphanumeric character
+\W	Any Non-alphanumeric character
+{m}	m Repetitions
+{m,n}	m to n Repetitions
+*	Zero or more repetitions
++	One or more repetitions
+?	Optional character
+\s	Any Whitespace
+\S	Any Non-whitespace character
+^…$	Starts and ends
+(…)	Capture Group
+(a(bc))	Capture Sub-group
+(.*)	Capture all
+(abc|def)	Matches abc or def
+'''
